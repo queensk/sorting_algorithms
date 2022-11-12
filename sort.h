f26eeb29df1337aad4ef929356fefe571e1e668f
queens kisivuli <queenskisivuli@gmail.com>
2022-11-13 01:02:21 +0300

    updated documetation

--- sort.h
+++ sort.h
@@ -21,9 +21,29 @@ typedef struct listint_s
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
 void bubble_sort(int *array, size_t size);
+int len_list(listint_t *h);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void quick_recursion(int *array, int left, int right, size_t size);
+int partition(int *array, int left, int right, size_t size);
+void shell_sort(int *array, size_t size);
+void switch_nodes(listint_t **list, listint_t **p);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge_recursion(int *arr, int *array, size_t left, size_t right);
+void merge_subarray(int *arr, int *array, size_t left,
+		size_t middle, size_t right);
+void heap_sort(int *array, size_t size);
+void heapify(int *array, size_t s, size_t root, size_t size);
+void radix_sort(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
+void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
+void bitonic_merge(int *array, int l, int r, int direction);
 
 #endif
