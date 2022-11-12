e0aba68ad07b6fcfc40ca506d0ae09298ccd6396
queens kisivuli <queenskisivuli@gmail.com>
2022-11-12 23:17:54 +0300

    fixed error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,30 +1,30 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * bubble_sort - sorts an array in ascending order
  * @array: array of ints to sort
  * @size: size of the array
  */
-void bubble_sort(int *array, size_t size);
+void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j;
-    int temp;
+	int tmp;
 
-    if (size < 2)
-        return
+	if (size < 2)
+		return;
 
-    for(i = 0; i < size; i++)
-    {
+	for (i = 0; i < size; i++)
+	{
 		for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
-        }
-    }
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
 }
